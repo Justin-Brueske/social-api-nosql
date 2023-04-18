e370c6c32c428ac0d2669e36ed8e211625e7973a
Justin-Brueske <119259409+Justin-Brueske@users.noreply.github.com>
2023-04-18 11:19:02 -0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,9 +38,16 @@ const userController = {
                 _id: {
                     $in: user.thoughts
                 }
-        })).then(() => res.json({ message: 'User and associated apps deleted!' })).catch((err) => res.status(500).json(err));
+        })).then(() => res.json({ message: 'User and associated apps deleted!' }))
+        .catch((err) => res.status(500).json(err));
     },
-
+    // getUserById,
+    getUserById(req, res) {
+        User.findOne({
+             _id: req.params.id })
+             .then((user) => !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user))
+             .catch((err) => res.status(500).json(err));
+    },
 };
 
     module.exports = userController;
